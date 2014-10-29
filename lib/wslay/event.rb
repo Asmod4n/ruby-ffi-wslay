5b4dd1f1404f1c8ed82322d77c4b72e010bcb057
Asmod4n <Asmod4n@users.noreply.github.com>
2014-10-29 11:54:30 +0100

    don't retain the GIL

--- lib/wslay/event.rb
+++ lib/wslay/event.rb
@@ -66,22 +66,22 @@ module Wslay
               :msg_length,  :size_t
     end
 
-    attach_function :read,  :wslay_event_recv,  [:pointer], Error
-    attach_function :write, :wslay_event_send,  [:pointer], Error
+    attach_function :read,  :wslay_event_recv,  [:pointer], Error, blocking: true
+    attach_function :write, :wslay_event_send,  [:pointer], Error, blocking: true
 
-    attach_function :queue_msg,   :wslay_event_queue_msg,   [:pointer, Msg.ptr],  Error
-    attach_function :queue_close, :wslay_event_queue_close, [:pointer, StatusCode, :buffer_in, :size_t],  Error
+    attach_function :queue_msg,   :wslay_event_queue_msg,   [:pointer, Msg.ptr],  Error, blocking: true
+    attach_function :queue_close, :wslay_event_queue_close, [:pointer, StatusCode, :buffer_in, :size_t],  Error, blocking: true
 
-    attach_function :set_error, :wslay_event_set_error, [:pointer, Error],  :void
+    attach_function :set_error, :wslay_event_set_error, [:pointer, Error],  :void, blocking: true
 
-    attach_function :want_read,   :wslay_event_want_read,   [:pointer], :bool
-    attach_function :want_write,  :wslay_event_want_write,  [:pointer], :bool
+    attach_function :want_read,   :wslay_event_want_read,   [:pointer], :bool, blocking: true
+    attach_function :want_write,  :wslay_event_want_write,  [:pointer], :bool, blocking: true
 
-    attach_function :get_close_received,        :wslay_event_get_close_received,        [:pointer], :bool
-    attach_function :get_close_sent,            :wslay_event_get_close_sent,            [:pointer], :bool
-    attach_function :get_status_code_received,  :wslay_event_get_status_code_received,  [:pointer], StatusCode
-    attach_function :get_status_code_sent,      :wslay_event_get_status_code_sent,      [:pointer], StatusCode
-    attach_function :get_queued_msg_count,      :wslay_event_get_queued_msg_count,      [:pointer], :size_t
-    attach_function :get_queued_msg_length,     :wslay_event_get_queued_msg_length,     [:pointer], :size_t
+    attach_function :get_close_received,        :wslay_event_get_close_received,        [:pointer], :bool, blocking: true
+    attach_function :get_close_sent,            :wslay_event_get_close_sent,            [:pointer], :bool, blocking: true
+    attach_function :get_status_code_received,  :wslay_event_get_status_code_received,  [:pointer], StatusCode, blocking: true
+    attach_function :get_status_code_sent,      :wslay_event_get_status_code_sent,      [:pointer], StatusCode, blocking: true
+    attach_function :get_queued_msg_count,      :wslay_event_get_queued_msg_count,      [:pointer], :size_t, blocking: true
+    attach_function :get_queued_msg_length,     :wslay_event_get_queued_msg_length,     [:pointer], :size_t, blocking: true
   end
 end
