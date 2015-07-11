1e19754844d14a37fcbce680479ec5025fccc603
Asmod4n <Asmod4n@users.noreply.github.com>
2015-07-11 10:38:07 +0200

    fix is_ctrl_frame and get_rsv

--- lib/wslay.rb
+++ lib/wslay.rb
@@ -5,19 +5,23 @@ module Wslay
   module_function
 
   def is_ctrl_frame(opcode)
-    ((opcode >> 3) & 1) == 1
+    opcode = Integer(opcode)
+    ((opcode >> 3) & 1) != 0
   end
 
   def get_rsv1(rsv)
-    ((rsv >> 2) & 1) == 1
+    rsv = Integer(rsv)
+    ((rsv >> 2) & 1)
   end
 
   def get_rsv2(rsv)
-    ((rsv >> 1) & 1) == 1
+    rsv = Integer(rsv)
+    ((rsv >> 1) & 1)
   end
 
   def get_rsv3(rsv)
-    (rsv & 1) == 1
+    rsv = Integer(rsv)
+    (rsv & 1)
   end
 end
 
